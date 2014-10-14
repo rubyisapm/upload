10b2b0aa41f8273ae2b59a8cd5ee4d2de4830fa4
skysunshine99 <skysunshine99@126.com>
2014-10-14 22:22:11 +0800

    little case

--- script/router/submit.js
+++ script/router/submit.js
@@ -5,18 +5,37 @@ var express=require('express');
 var router=express.Router();
 var queryString=require('querystring');
 var fs=require('fs');
+var formidable=require('formidable');
 router.use(function(req,res){
-    var receiveData='';
-    req.on('data',function(data){
-        receiveData+=data;
-    })
-    req.on('end',function(){
-        var data=queryString.parse(receiveData);
-        var picPath=data.picture;
-/*        fs.readlink(picPath,function(error,linkString){
-            console.log(linkString);
-        })*/
-        res.close();
-    })
+
+
+    var form = new formidable.IncomingForm();
+    var post = {},
+        file = {};
+    form.uploadDir = '/tmp';
+
+    form.on('error', function(err) {
+            console.log(err);
+        }).on('field', function(field, value) {
+            if (form.type == 'multipart') {
+                if (field in post) {
+                    if (util.isArray(post[field]) === false) {
+                        post[field] = [post[field]];
+                    }
+                    post[field].push(value);
+                    return;
+                }
+            }
+            post[field] = value;
+        }).on('file', function(field, file) {
+            file[field] = file;
+        }).on('end', function() {
+            console.log('ok');
+        });
+    form.parse(req,function(err,fields,files){
+        req.body=fields;
+        req.files=files;
+        console.log(req.body);
+    });
 })
 exports.router=router;
--- script/web/submit.js
+++ script/web/submit.js
@@ -1,16 +1,42 @@
 /**
  * Created by ruby on 2014/10/14.
  */
-$("#submit").click(function(){
-    var param={
-        name:$('#name').val(),
-        picture:$('#picture').val()
+function readfile(f){
+    var reader=new FileReader();
+    reader.readAsBinaryString(f);
+    reader.onload=function(){
+        var text=reader.result;
+        return text;
     }
-    $.ajax({
+    reader.onerror=function(e){
+        console.log(e);
+    }
+}
+document.querySelector("#submit").onclick=function(){
+
+    var request=new XMLHttpRequest();
+    request.open('POST','/submit');
+    request.onreadystatechange=function(){
+        if(request.readyState===4){
+        }
+    }
+    var param={
+        name:document.querySelector('#name').value,
+        picture:document.querySelector('#picture').files
+    }
+    var formData=new FormData();
+    for(var name in param){
+        if(!param.hasOwnProperty(name)) continue;
+        var value=param[name];
+        if(typeof value ==='function') continue;
+        formData.append(name,value);
+    }
+    request.send(formData);
+    /*$.ajax({
         url:'/submit',
         type:'POST',
         async:false,
-        data:param,
+        data:formData,
         contentType:'multipart/form-data',
         success:function(data){
             console.log(data);
@@ -18,5 +44,7 @@ $("#submit").click(function(){
         error:function(){
             console.log('error!');
         }
-    })
-})
+    })*/
+    //readfile(document.querySelector('#picture').files[0]);
+
+}
