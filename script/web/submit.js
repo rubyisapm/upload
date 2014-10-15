8b754c53b84e4f900bccdfa8944346a2d3320a71
skysunshine99 <skysunshine99@126.com>
2014-10-15 17:19:36 +0800

    little case

--- script/web/submit.js
+++ script/web/submit.js
@@ -1,17 +1,6 @@
 /**
  * Created by ruby on 2014/10/14.
  */
-function readfile(f){
-    var reader=new FileReader();
-    reader.readAsBinaryString(f);
-    reader.onload=function(){
-        var text=reader.result;
-        return text;
-    }
-    reader.onerror=function(e){
-        console.log(e);
-    }
-}
 document.querySelector("#submit").onclick=function(){
     var request=new XMLHttpRequest();
     request.open('POST','/submit');
